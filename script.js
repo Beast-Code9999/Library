793c02c506bc8020e2e540984ced096cae08baf9
Beast Code <beastcode9999@gmail.com>
2022-10-10 21:28:53 +0900

    add input variables in script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const inputTitle = document.getElementById('input-title');
+const inputAuthor = document.getElementById('input-author');
+const inputPages = documnet.getElementById('input-pages');
+console.log(inputAuthor)
+
+
+
+
 let myLibrary = [];
 
 function Book() {
