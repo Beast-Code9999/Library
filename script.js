afb7139b865a6f62784bc0a4cc8ae9a905a0f51b
Beast Code <beastcode9999@gmail.com>
2022-10-10 21:36:55 +0900

    add inputDate and inputRead in script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const inputTitle = document.getElementById('input-title');
 const inputAuthor = document.getElementById('input-author');
 const inputPages = documnet.getElementById('input-pages');
 console.log(inputAuthor)
-
+const inputDate = document.getElementById('input-date');
+const inputRead = document.getElementById('input-')
 
 
 
