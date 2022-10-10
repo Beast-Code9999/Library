4ee9c1cff267174c782e1662b84fb388b3fc67f3
Beast Code <beastcode9999@gmail.com>
2022-10-10 21:43:06 +0900

    add selector of total number in header

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+// start of sidebar selection
 const inputTitle = document.getElementById('input-title');
 const inputAuthor = document.getElementById('input-author');
-const inputPages = documnet.getElementById('input-pages');
+const inputPages = document.getElementById('input-pages');
 
 const inputDate = document.getElementById('input-date');
-const inputRead = document.getElementById('input-');
+const inputRead = document.getElementById('input-read');
 
 const addBtn = document.getElementById('add-button');
-
-
+// end of sidebar selection
+//start of header selection
+const totalNum = document.getElementById('total-num');
+console.log(totalNum)
 
 
 let myLibrary = [];
