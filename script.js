e6c6086738dddf0481b28e227b443015104e08cf
Beast Code <beastcode9999@gmail.com>
2022-10-10 21:52:13 +0900

    add position fixed css style t .aside

--- script.js
+++ script.js
@@ -2,10 +2,8 @@
 const inputTitle = document.getElementById('input-title');
 const inputAuthor = document.getElementById('input-author');
 const inputPages = document.getElementById('input-pages');
-
 const inputDate = document.getElementById('input-date');
 const inputRead = document.getElementById('input-read');
-
 const addBtn = document.getElementById('add-button');
 // end of sidebar selection
 //start of header selection
@@ -14,6 +12,8 @@ const readNum = document.getElementById('read-num');
 const unreadNum = document.getElementById('unread-num');
 const deleteAll = document.getElementById('delete-all');
 // end of header selection
+// start main selection
+
 
 let myLibrary = [];
 
