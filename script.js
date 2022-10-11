74404ac527be8d9838f4036aaf4ad7e5276d1b41
Beast Code <beastcode9999@gmail.com>
2022-10-11 20:21:25 +0900

    add javaScript to dynamically add or remove .input__text-notnull to each input__text

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const inputPages = document.getElementById('input-pages');
 const inputDate = document.getElementById('input-date');
 const inputRead = document.getElementById('input-read');
 const addBtn = document.getElementById('add-button');
+const inputText = document.querySelectorAll('.input__text')
 // end of sidebar selection
 //start of header selection
 const totalNum = document.getElementById('total-num');
@@ -13,6 +14,21 @@ const unreadNum = document.getElementById('unread-num');
 const deleteAll = document.getElementById('delete-all');
 // end of header selection
 // start main selection
+
+// end of mmain selection
+
+
+// input label style
+inputText.forEach( item => {
+  item.addEventListener('input', () => {
+    if(item.value !== "") {
+      item.nextElementSibling.classList.add('input__text-notnull')
+    }
+    else {
+      item.nextElementSibling.classList.remove('input__text-notnull')
+    }
+  })
+})
 
 
 let myLibrary = [];
